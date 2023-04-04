36ae0800cb5a23500dca4437ff55a1ee8be52050
k2262470 <konstantin.roeder@kcl.ac.uk>
2023-04-04 13:33:40 +0100

    New commons module for globals

---
+++ commons.f90
@@ -0,0 +1,26 @@
+MODULE COMMONS
+   !> real, double precision 64-bit
+   INTEGER, PARAMETER  :: REAL64 = SELECTED_REAL_KIND(15, 307)  
+   ! number of residues
+   INTEGER :: NRES = 0
+   ! residue name
+   CHARACTER(LEN=4), ALLOCATABLE :: RESNAMES(:)
+   ! number of atoms
+   INTEGER :: NATOMS = 0
+   ! atom names
+   CHARACTER(LEN=4), ALLOCATABLE :: ATOMNAMES(:)
+   ! type for atoms in residue
+   TYPE RESIDUE
+      INTEGER :: NATS = 0
+      INTEGER, ALLOCATABLE :: ATOMIDS(:)
+      CHARACTER(LEN=4), ALLOCATABLE :: ATOMNAMES(:)
+   END TYPE RESIDUE
+   TYPE(RESIDUE), ALLOCATABLE :: ATOMDATA(:)
+   INTEGER, ALLOCATABLE :: FIRSTAT(:)
+   INTEGER, ALLOCATABLE :: LASTAT(:)
+
+   !> atoms for the helical axis
+   INTEGER :: NPOINT1, NPOINT2
+   INTEGER, ALLOCATABLE :: ATSFOR1(:), ATSFOR2(:)
+
+END MODULE COMMONS
